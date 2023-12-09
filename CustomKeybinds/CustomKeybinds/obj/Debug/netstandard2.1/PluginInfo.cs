218f77a053cd3b48bd73ec0ecd9e5cdf0557b514
Josh <holtz185@umn.edu>
2023-12-08 23:19:32 -0600

    Initial commit & rename

---
+++ CustomKeybinds/CustomKeybinds/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CustomKeybinds
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "CustomKeybinds";
+        public const string PLUGIN_NAME = "CustomKeybinds";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
